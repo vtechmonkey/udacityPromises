bb8fcfd2498442ad93cfccc45a48922034738b98
vtechmonkey <sarah.oconnor@webinnovate.ie>
2016-06-14 11:55:19 +0100

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,121 @@
+/*
+Instructions:
+(1) Refactor .forEach below to create a sequence of Promises that always resolves in the same
+    order it was created.
+  (a) Fetch each planet's JSON from the array of URLs in the search results.
+  (b) Call createPlanetThumb on each planet's response data to add it to the page.
+(2) Use developer tools to determine if the planets are being fetched in series or in parallel.
+ */
+
+// Inline configuration for jshint below. Prevents `gulp jshint` from failing with quiz starter code.
+/* jshint unused: false */
+
+(function(document) {
+  'use strict';
+
+  var home = null;
+
+  /**
+   * Helper function to show the search query.
+   * @param {String} query - The search query.
+   */
+  function addSearchHeader(query) {
+    home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
+  }
+
+  /**
+   * Helper function to create a planet thumbnail.
+   * @param  {Object} data - The raw data describing the planet.
+   */
+  function createPlanetThumb(data) {
+    var pT = document.createElement('planet-thumb');
+    for (var d in data) {
+      pT[d] = data[d];
+    }
+    home.appendChild(pT);
+  }
+
+  /**
+   * XHR wrapped in a promise.
+   * @param  {String} url - The URL to fetch.
+   * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
+   */
+  function get(url) {
+    return fetch(url, {
+      method: 'get'
+    });
+  }
+
+  /**
+   * Performs an XHR for a JSON and returns a parsed JSON response.
+   * @param  {String} url - The JSON URL to fetch.
+   * @return {Promise}    - A promise that passes the parsed JSON response.
+   */
+  function getJSON(url) {
+    return get(url).then(function(response) {
+      return response.json();
+    });
+  }
+
+  window.addEventListener('WebComponentsReady', function() {
+    home = document.querySelector('section[data-route="home"]');
+    
+    //Refactor this code!
+    // returns urls in parallel
+    //   getJSON('../data/earth-like-results.json')
+    //   .then(function(response) {
+    //     response.results.forEach(function(url) {
+    //     getJSON(url).then(createPlanetThumb);
+    //     });
+    //     });
+    
+     
+     //returns urls in series 
+   //  getJSON('../data/earth-like-results.json')
+   //  .then(function(response) {
+   //    var sequence = Promise.resolve();
+   //    addSearchHeader(response.query);
+   //    response.results.forEach(function(url){
+   //      sequence = sequence.then(function(){
+   //      return getJSON(url);
+   //      })
+   //    .then(createPlanetThumb);
+   //    });    
+   //  })
+   //  .catch(function(e){
+   //    console.log(e);
+   // });
+
+
+    // return urls using .map
+    /*
+  Instructions:
+  (1) Use .map to fetch all the planets in parallel.
+    (a) Call .map on an array and pass it a function.
+    (b) .map will execute the function against each element
+         in the array immediately.
+   */
+  // getJSON('../data/earth-like-results.json')
+  // .then(function(response){
+  //   var planets = response.results;
+  //   addSearchHeader(response.query);
+  //   planets.map(function(url){
+  //     getJSON(url).then(createPlanetThumb);
+  //   });
+  getJSON('../data/earth-like-results.json')
+  .then(function(response){
+    addSearchHeader(response.query);
+    return Promise.all(response.results.map(getJSON));
+  })
+  .then(function(planetData){
+    planetData.forEach(function(planet){
+      createPlanetThumb(planet);
+    });
+  })
+  .catch(function(error){
+    console.log(error);
+  });
+  
+  
+  });
+})(document);
